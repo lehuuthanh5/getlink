046fc9908103a855d9a66dfacb6ba66ab8e898d6
lehuuthanh5 <huuthanh.qt.spkt@gmail.com>
2019-02-22 14:16:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 		 <div class="row" style = "margin-top: 18px;">
             <div class="col-8 offset-md-4">
                 <div class="col-12 input-group">
-                   <div class="g-recaptcha" data-sitekey="6LcSJ5MUAAAAAIpQu6x_kNZeZupwHPqANolF1Ivt"></div>
+                   <div class="g-recaptcha" data-sitekey="6LfbK5MUAAAAAL5aOTlbuaNjM7yj8TbBDJYCinzV"></div>
                 </div>
             </div>
         </div>
